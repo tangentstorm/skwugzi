82a1af96f10e54a08555d8cb57f02a75277deeb3
tangentstorm <michal.wallace@gmail.com>
2023-04-05 21:13:58 -0400

    implement DrawChar(x,y,c,fg,bg) with anti-aliased bitmaps

--- unit1.pas
+++ unit1.pas
@@ -20,9 +20,11 @@ type
     procedure FormDestroy(Sender: TObject);
     procedure FormPaint(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
+    procedure DrawChar(x,y:integer; c:char; fg,bg:TColor);
   private
     bmp:TBGRABitmap;
     fnt:Array[byte] of TBGRABitmap;
+    tmp:TBGRABitmap;
   public
 
   end;
@@ -50,6 +52,7 @@ begin
     FontAntialias:= true;
     TextOut(1,0, chr(i), BGRAWhite);
   end;
+  tmp := TBGRABitmap.Create(CW,CH, BGRAPixelTransparent);
 end;
 
 procedure TForm1.FormClick(Sender: TObject);
@@ -69,10 +72,24 @@ begin
   gx := (ClientWidth  mod cw) div 2; gw := ClientWidth  div cw;
   gy := (ClientHeight mod ch) div 2; gh := ClientHeight div ch;
   for j := 0 to gh-1 do for i := 0 to gw-1 do begin
-    bmp.FillRect(gx+i*CW, gy+j*CH, gx+(i+1)*CW, gy+(j+1)*CH, TColor($7f7f7f and Random($ffffff)));
-    bmp.PutImage(gx+i*CW, gy+j*CH, fnt[33+byte(Random(94))], dmDrawWithTransparency);
+    drawChar(gx+i*CW, gy+j*CH, chr(33+byte(Random(94))),
+             TColor(Random($ffffff)), BGRABlack); //TColor(Random($ffffff))); //);
   end;
   bmp.Draw(Canvas, 0, 0, true);
+end;
+
+procedure TForm1.DrawChar(x,y:integer; c:char; fg,bg:TColor);
+var r,w: PBGRAPixel; i:word;
+begin
+  bmp.FillRect(x, y, x+CW, y+CH, bg);
+  // make a new colored char with alpha
+  r := fnt[byte(c)].Data; w := tmp.Data;
+  for i := tmp.NbPixels-1 downto 0 do begin
+    w^ := fg;  w^.alpha := r^.alpha;
+    inc(r); inc(w);
+  end;
+  tmp.InvalidateBitmap;
+  bmp.PutImage(x, y, tmp, dmDrawWithTransparency);
 end;
 
 
